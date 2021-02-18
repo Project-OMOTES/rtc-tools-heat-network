cbe05c42a7147c97a07f70a57aafd747fa05dff7
Tjerk Vreeken <tjerk.vreeken@deltares.nl>
2021-02-18 23:52:57 +0100

    Tests/double_pipe_heat: Remove lingering debug code
    The directions code was just copy/pasted from util.py. As "proof" that
    it is debug code, it includes the stereotypical `a = 1` at the end for
    having a line to set a breakpoint on.

--- tests/models/double_pipe_heat/src/double_pipe_heat.py
+++ tests/models/double_pipe_heat/src/double_pipe_heat.py
@@ -3,7 +3,6 @@ from rtctools.optimization.collocated_integrated_optimization_problem import (
 )
 from rtctools.optimization.csv_mixin import CSVMixin
 from rtctools.optimization.goal_programming_mixin import Goal, GoalProgrammingMixin
-from rtctools.optimization.timeseries import Timeseries
 from rtctools.util import run_optimization_problem
 
 from rtctools_heat_network.heat_mixin import HeatMixin
@@ -66,34 +65,3 @@ class DoublePipeEqualHeat(
 
 if __name__ == "__main__":
     heat_problem = run_optimization_problem(DoublePipeEqualHeat)
-
-    results = heat_problem.extract_results()
-    times = heat_problem.times()
-
-    directions = {}
-
-    for p in heat_problem.hot_pipes:
-        heat_in = results[p + ".HeatIn.Heat"]
-        heat_out = results[p + ".HeatOut.Heat"]
-
-        if not heat_problem.parameters(0)[p + ".disconnectable"]:
-            # Flow direction is directly related to the sign of the heat
-            direction_pipe = (heat_in >= 0.0).astype(int) * 2 - 1
-        elif heat_problem.parameters(0)[p + ".disconnectable"]:
-            direction_pipe = (heat_in >= 0.0).astype(int) * 2 - 1
-            # Disconnect a pipe when the heat entering the component is only used
-            # to account for its heat losses. There are three cases in which this
-            # can happen.
-            direction_pipe[((heat_in > 0.0) & (heat_out < 0.0))] = 0
-            direction_pipe[((heat_in < 0.0) & (heat_out > 0.0))] = 0
-            direction_pipe[((heat_in == 0.0) | (heat_out == 0.0))] = 0
-        directions[p] = Timeseries(times, direction_pipe)
-
-        # NOTE: The assumption is that the orientation of the cold pipes is such that the flow
-        # is always in the same direction as its "hot" pipe companion.
-        cold_pipe = heat_problem.hot_to_cold_pipe(p)
-        directions[cold_pipe] = directions[p]
-
-    directions_values = {k: v.values for k, v in directions.items()}
-
-    a = 1
