279fd7eeb91982070584d0b236af2b2d048eca60
Tjerk Vreeken <tjerk.vreeken@deltares.nl>
2021-06-11 11:20:39 +0200

    Tests: Use pytest to parameterize tests of examples
    The test of the examples are dynamically generated based on the contents
    of the examples folder. Before this commit, they were all part of one
    single test. It was therefore hard/impossible to parallelize (with e.g.
    pytest-xdist), and it also meant the entire test would fail if a single
    example had an error. Further inspection of the logs was necessary to
    figure out which example(s) had failed.
    
    Because we are mostly relying on pytest anyway during CI/CD, and
    checking the exit code of the examples is more of a "nice to have"
    compared to the other tests, we deviate from the norm of writing all
    tests in unittest format. The benefits of parallelization, logging and
    cleaner code outweigh the downsides of relying on a particular testing
    package.

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,58 +1,17 @@
-import inspect
-import os
 import subprocess
 import sys
-from unittest import TestCase
+from pathlib import Path
+
+import pytest
+
+examples = list((Path(__file__).resolve().parent.parent / "examples").glob("**/src/example.py"))
 
 
-class ExamplesCollection:
-    def __init__(self):
-        self.errors_detected = {}
-        for example_folder in self.examples_folders:
-            # initialize with failures
-            example_path = os.path.join(self.examples_path, example_folder, "src", "example.py")
-            self.errors_detected[example_path] = True
-
-    def local_function(self):
-        pass
-
-    @property
-    def examples_path(self):
-        return os.path.join(
-            os.path.dirname(os.path.abspath(inspect.getsourcefile(self.local_function))),
-            "..",
-            "examples",
-        )
-
-    @property
-    def examples_folders(self):
-        folders = []
-        for dirpath, dirnames, _ in os.walk(self.examples_path):
-            if "src" in dirnames:
-                folders.append(os.path.relpath(dirpath, self.examples_path))
-        return folders
-
-
-class TestExamples(TestCase):
-    def run_examples(self, ec):
-        env = sys.executable
-        for example_path in ec.errors_detected.keys():
-            try:
-                subprocess.check_output([env, example_path])
-            except Exception:
-                ec.errors_detected[example_path] = True
-            else:
-                ec.errors_detected[example_path] = False
-
-    def test_examples(self):
-        ec = ExamplesCollection()
-
-        self.run_examples(ec)
-
-        for example_path, error_detected in ec.errors_detected.items():
-            if error_detected:
-                print("An error occured while running example '{}'.".format(example_path))
-            else:
-                print("No errors occured while running example '{}'.".format(example_path))
-
-        self.assertFalse(any(ec.errors_detected.values()))
+@pytest.mark.parametrize("example", examples, ids=[x.parent.parent.name for x in examples])
+def test_examples(example):
+    example_name = example.parent.parent.name
+    env = sys.executable
+    try:
+        subprocess.check_output([env, example])
+    except Exception:
+        pytest.fail(f"Running example '{example_name}' failed")
