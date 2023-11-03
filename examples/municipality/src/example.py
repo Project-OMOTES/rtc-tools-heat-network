a48cb30f808262389d828923df4f32cbe2c2bc53
Femke Janssen <femke.janssen@tno.nl>
2023-11-03 10:57:55 +0100

    added example to test municipality

---
+++ examples/municipality/src/example.py
@@ -0,0 +1,13 @@
+from rtctools.util import run_optimization_problem
+
+from rtctools_heat_network.workflows import EndScenarioSizingHIGHS
+
+if __name__ == "__main__":
+    import time
+
+    start_time = time.time()
+
+    heat_problem = run_optimization_problem(EndScenarioSizingHIGHS)
+    results = heat_problem.extract_results()
+
+    print("Execution time: " + time.strftime("%M:%S", time.gmtime(time.time() - start_time)))
