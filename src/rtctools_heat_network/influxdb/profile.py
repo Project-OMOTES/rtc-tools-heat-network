c758a866f3540b1404a852a0a9809d8b86f06e86
kobus.vanrooyen@tno.nl <kobus.vanrooyen@tno.nl>
2023-11-13 11:48:32 +0100

    Update unit conversion with pyESDL function

--- src/rtctools_heat_network/influxdb/profile.py
+++ src/rtctools_heat_network/influxdb/profile.py
@@ -5,6 +5,8 @@ from datetime import timedelta as td
 import esdl
 from esdl.profiles.influxdbprofilemanager import ConnectionSettings
 from esdl.profiles.influxdbprofilemanager import InfluxDBProfileManager
+from esdl.units.conversion import ENERGY_IN_J, POWER_IN_W, convert_to_unit
+
 
 import pandas as pd
 
@@ -16,73 +18,73 @@ time_step = td(seconds=3600)
 time_step_notation = "{}s".format(int(time_step.total_seconds()))
 
 
-def get_mw_multiplier(profile: esdl.GenericProfile):
-    ts = time_step.total_seconds()
-    factor = {
-        "ENERGY_IN_WH": 3.6 / (1e3 * ts),
-        "ENERGY_IN_KWH": 3.6 / ts,
-        "ENERGY_IN_MWH": (3.6 * 1e3) / ts,
-        "ENERGY_IN_GWH": (3.6 * 1e6) / ts,
-        "ENERGY_IN_TWH": (3.6 * 1e9) / ts,
-        "ENERGY_IN_PWH": (3.6 * 1e12) / ts,
-        "ENERGY_IN_J": 1 / (1e6 * ts),
-        "ENERGY_IN_KJ": 1 / (1e3 * ts),
-        "ENERGY_IN_MJ": 1 / ts,
-        "ENERGY_IN_GJ": 1e3 / ts,
-        "ENERGY_IN_TJ": 1e6 / ts,
-        "ENERGY_IN_PJ": 1e9 / ts,
-        "POWER_IN_W": 1 / 1e6,
-        "POWER_IN_KW": 1 / 1e3,
-        "POWER_IN_MW": 1,
-        "POWER_IN_GW": 1e3,
-        "POWER_IN_TW": 1e6,
-        "POWER_IN_PW": 1e9,
-    }
+# def get_mw_multiplier(profile: esdl.GenericProfile):
+#     ts = time_step.total_seconds()
+#     factor = {
+#         "ENERGY_IN_WH": 3.6 / (1e3 * ts),
+#         "ENERGY_IN_KWH": 3.6 / ts,
+#         "ENERGY_IN_MWH": (3.6 * 1e3) / ts,
+#         "ENERGY_IN_GWH": (3.6 * 1e6) / ts,
+#         "ENERGY_IN_TWH": (3.6 * 1e9) / ts,
+#         "ENERGY_IN_PWH": (3.6 * 1e12) / ts,
+#         "ENERGY_IN_J": 1 / (1e6 * ts),
+#         "ENERGY_IN_KJ": 1 / (1e3 * ts),
+#         "ENERGY_IN_MJ": 1 / ts,
+#         "ENERGY_IN_GJ": 1e3 / ts,
+#         "ENERGY_IN_TJ": 1e6 / ts,
+#         "ENERGY_IN_PJ": 1e9 / ts,
+#         "POWER_IN_W": 1 / 1e6,
+#         "POWER_IN_KW": 1 / 1e3,
+#         "POWER_IN_MW": 1,
+#         "POWER_IN_GW": 1e3,
+#         "POWER_IN_TW": 1e6,
+#         "POWER_IN_PW": 1e9,
+#     }
 
-    mult_suffix = {
-        "NONE": "",
-        "KILO": "K",
-        "MEGA": "M",
-        "GIGA": "G",
-        "TERRA": "T",
-        "PETA": "P",
-        "MILLI": "-",
-        "MICRO": "-",
-        "NANO": "-",
-        "PICO": "-",
-    }
+#     mult_suffix = {
+#         "NONE": "",
+#         "KILO": "K",
+#         "MEGA": "M",
+#         "GIGA": "G",
+#         "TERRA": "T",
+#         "PETA": "P",
+#         "MILLI": "-",
+#         "MICRO": "-",
+#         "NANO": "-",
+#         "PICO": "-",
+#     }
 
-    if profile.profileType is not None and profile.profileType != esdl.ProfileTypeEnum.UNDEFINED:
-        if profile.profileType.name not in factor:
-            print("Unsupported profile type : {}".format(profile.profileType))
-            return 0
-        return factor[profile.profileType.name]
+#     if profile.profileType is not None and profile.profileType != esdl.ProfileTypeEnum.UNDEFINED:
+#         if profile.profileType.name not in factor:
+#             print("Unsupported profile type : {}".format(profile.profileType))
+#             return 0
+#         return factor[profile.profileType.name]
 
-    if isinstance(profile.profileQuantityAndUnit, esdl.QuantityAndUnitReference):
-        p = profile.profileQuantityAndUnit.reference
-    elif isinstance(profile.profileQuantityAndUnit, esdl.QuantityAndUnitType):
-        p = profile.profileQuantityAndUnit
+#     if isinstance(profile.profileQuantityAndUnit, esdl.QuantityAndUnitReference):
+#         p = profile.profileQuantityAndUnit.reference
+#     elif isinstance(profile.profileQuantityAndUnit, esdl.QuantityAndUnitType):
+#         p = profile.profileQuantityAndUnit
 
-    if p is not None:
-        if p.unit == esdl.UnitEnum.JOULE:
-            phy_quantity = "ENERGY_IN_"
-            suffix = "J"
-        elif p.unit == esdl.UnitEnum.WATTHOUR:
-            phy_quantity = "ENERGY_IN_"
-            suffix = "WH"
-        elif p.unit == esdl.UnitEnum.WATT:
-            phy_quantity = "POWER_IN_"
-            suffix = "W"
-        else:
-            print("Unsupported unit : {}".format(p.unit))
-            return 0
+#     if p is not None:
+#         if p.unit == esdl.UnitEnum.JOULE:
+#             phy_quantity = "ENERGY_IN_"
+#             suffix = "J"
+#         elif p.unit == esdl.UnitEnum.WATTHOUR:
+#             phy_quantity = "ENERGY_IN_"
+#             suffix = "WH"
+#         elif p.unit == esdl.UnitEnum.WATT:
+#             phy_quantity = "POWER_IN_"
+#             suffix = "W"
+#         else:
+#             print("Unsupported unit : {}".format(p.unit))
+#             return 0
 
-        prefix = mult_suffix[p.multiplier.name]
-        if prefix == "-":
-            print("Unsupported multiplier : {}".format(p.multiplier))
-            return 0
+#         prefix = mult_suffix[p.multiplier.name]
+#         if prefix == "-":
+#             print("Unsupported multiplier : {}".format(p.multiplier))
+#             return 0
 
-        return factor["{}{}{}".format(phy_quantity, prefix, suffix)]
+#         return factor["{}{}{}".format(phy_quantity, prefix, suffix)]
 
 
 def parse_esdl_profiles(es, start_date=None, end_date=None):
@@ -91,7 +93,8 @@ def parse_esdl_profiles(es, start_date=None, end_date=None):
     for profile in [x for x in es.eAllContents() if isinstance(x, esdl.InfluxDBProfile)]:
         profile_host = profile.host
         containing_asset_id = profile.eContainer().energyasset.id
-        to_mw_multiplier = get_mw_multiplier(profile)
+        # to_mw_multiplier = get_mw_multiplier(profile)
+
         ssl_setting = False
         if "https" in profile_host:
             profile_host = profile_host[8:]
@@ -144,7 +147,38 @@ def parse_esdl_profiles(es, start_date=None, end_date=None):
         }
         df = pd.DataFrame.from_dict(data_points, orient="index")
         df.index = pd.to_datetime(df.index, utc=True)
-        data_set[containing_asset_id] = df * profile.multiplier * to_mw_multiplier * 1.0e6
+        # TODO: remove old code below once test case with profiles have been included
+        # data_set[containing_asset_id] = df * profile.multiplier * to_mw_multiplier * 1.0e6
+
+        # TODO add test case. Currently no test case for esdl parsing
+        # Convert Power and Energy to standard unit of Watt and Joules
+        # Note: at the time of this code below being implemented pyESDL catered for limited
+        # power/energy units which is in the process of being expanded
+        for idf in range(len(df)):
+            if (
+                profile.profileQuantityAndUnit.reference.physicalQuantity
+                == esdl.PhysicalQuantityEnum.POWER
+            ):
+                df.iloc[idf] = convert_to_unit(
+                    df.iloc[idf], profile.profileQuantityAndUnit, POWER_IN_W
+                )
+            elif (
+                profile.profileQuantityAndUnit.reference.physicalQuantity
+                == esdl.PhysicalQuantityEnum.ENERGY
+            ):
+                df.iloc[idf] = convert_to_unit(
+                    df.iloc[idf], profile.profileQuantityAndUnit, ENERGY_IN_J
+                )
+            else:
+                print(
+                    f"Current the code only caters for: {esdl.PhysicalQuantityEnum.POWER} & "
+                    f"{esdl.PhysicalQuantityEnum.ENERGY}, and it does not cater for "
+                    f"{profile.profileQuantityAndUnit.reference.physicalQuantity}"
+                )
+                sys.exit(1)
+
+        data_set[containing_asset_id] = df * profile.multiplier
+
         if len(error_neighbourhoods) > 0:
             raise RuntimeError(f"Encountered errors loading data for {error_neighbourhoods}")
 
