fixing scaling issue after Heat_in and Heat_out variables removed

tests/test_multiple_in_and_out_port_components.py
@@ -37,7 +37,7 @@ class TestHEX(TestCase):
         # Note that we are not testing the last element as we exploit the last timestep for
         # checking the disabled boolean and the assert statement doesn't work for a difference of
         # zero
-        np.testing.assert_allclose(prim_heat[-1], 0.0, atol=1e-9)
+        np.testing.assert_allclose(prim_heat[-1], 0.0, atol=1e-8)
         np.testing.assert_allclose(disabled[-1], 1.0)
         np.testing.assert_allclose(disabled[:-1], 0.0)
         # Check that heat is flowing through the hex