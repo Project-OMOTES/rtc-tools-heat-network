ee0c53ed6a60dd17652076fe4e368cd925e887a4
kobus.vanrooyen@tno.nl <kobus.vanrooyen@tno.nl>
2023-11-23 11:14:34 +0100

    Added all consumer and producer types & formatting

--- src/rtctools_heat_network/workflows/io/write_output.py
+++ src/rtctools_heat_network/workflows/io/write_output.py
@@ -500,8 +500,8 @@ class ScenarioOutput(HeatMixin):
         # TODO: Investigate if no cost in the ESDL then this breaks ESDL visibility
         total_energy_produced_locally_wh = {}
         total_energy_consumed_locally_wh = {}
-        estimated_energy_consumed_locally_wh = {}
-        estimated_energy_consumed_regional_wh = {}
+        estimated_energy_from_local_source_perc = {}
+        estimated_energy_from_regional_source_perc = {}
 
         for subarea in energy_system.instance[0].area.area:
             area_investment_cost = 0.0
@@ -556,15 +556,8 @@ class ScenarioOutput(HeatMixin):
                         self._asset_fixed_operational_cost_map[asset_name]
                     ][0]
 
-                    # ....sg
-                    if (
-                        asset_type == "HeatProducer"
-                        or asset_type == "GenericProducer"
-                        or asset_type == "ResidualHeatSource"
-                        or asset_type == "GeothermalSource"
-                        or asset_type == "ResidualHeatSource"
-                        or asset_type == "GasHeater"
-                    ):
+                    # Calculate the total energy [Wh] consumed/produced in an area
+                    if asset_name in self.heat_network_components.get("source", []):
                         try:
                             total_energy_produced_locally_wh[subarea.name] += np.sum(
                                 results[f"{asset_name}.Heat_source"][1:]
@@ -577,48 +570,63 @@ class ScenarioOutput(HeatMixin):
                                 * (self.times()[1:] - self.times()[0:-1])
                                 / 3600.0
                             )
+                    if asset_name in self.heat_network_components.get("demand", []):
+                        flow_variable = results[f"{asset_name}.Heat_demand"][1:]
+                    elif asset_name in self.heat_network_components.get("buffer", []):
+                        flow_variable = results[f"{asset_name}.Heat_buffer"][1:]
+                    elif asset_name in self.heat_network_components.get("ates", []):
+                        flow_variable = results[f"{asset_name}.Heat_ates"][1:]
+                    elif asset_name in self.heat_network_components.get("pipe", []):
+                        flow_variable = (
+                            np.ones(len(self.times())) * results[f"{asset_name}__hn_heat_loss"]
+                        )
+                    else:
+                        flow_variable = ""
                     if (
-                        asset_type == "HeatingDemand"
+                        asset_name in self.heat_network_components.get("demand", [])
+                        or asset_name in self.heat_network_components.get("buffer", [])
+                        or asset_name in self.heat_network_components.get("ates", [])
+                        or asset_name in self.heat_network_components.get("pipe", [])
                     ):
                         try:
-                            temp1 = 0.0
                             total_energy_consumed_locally_wh[subarea.name] += np.sum(
-                                results[f"{asset_name}.Heat_demand"][1:]
-                                * (self.times()[1:] - self.times()[0:-1])
-                                / 3600.0
+                                flow_variable * (self.times()[1:] - self.times()[0:-1]) / 3600.0
                             )
                         except KeyError:
                             total_energy_consumed_locally_wh[subarea.name] = np.sum(
-                                results[f"{asset_name}.Heat_demand"][1:]
-                                * (self.times()[1:] - self.times()[0:-1])
-                                / 3600.0
+                                flow_variable * (self.times()[1:] - self.times()[0:-1]) / 3600.0
                             )
-                            temp = 0.0
-
+                    # end Calculate the total energy consumed/produced in an area
                 # end if placed loop
             # end asset loop
 
-
+            # Calculate the estimated energy source [%] for an area
             try:
-                total_energy_produced_locally_wh_area = (
-                    total_energy_produced_locally_wh[subarea.name]
-                )
+                total_energy_produced_locally_wh_area = total_energy_produced_locally_wh[
+                    subarea.name
+                ]
             except KeyError:
                 total_energy_produced_locally_wh_area = 0.0
 
-            estimated_energy_consumed_locally_wh[subarea.name] = (
-                total_energy_produced_locally_wh_area
-                / total_energy_consumed_locally_wh[subarea.name] * 100.0
-            )
-            estimated_energy_consumed_regional_wh[subarea.name] = (
-                (1.0 - estimated_energy_consumed_locally_wh[subarea.name]) * 100.0
-            )
+            try:
+                estimated_energy_from_local_source_perc[subarea.name] = min(
+                    total_energy_produced_locally_wh_area
+                    / total_energy_consumed_locally_wh[subarea.name]
+                    * 100.0,
+                    100.0,
+                )
+                estimated_energy_from_regional_source_perc[subarea.name] = min(
+                    (100.0 - estimated_energy_from_local_source_perc[subarea.name]), 100.0
+                )
+            except KeyError:
+                # Nothing to do, go on to next section of code
+                pass
 
-
-            # Here we add KPIs to the polygon area which allows to visualize them by hoovering over
+            # Here we add KPIs to the polygon area which allows to visualize them by hovering over
             # it with the mouse
             # Only update kpis if one of the costs > 0, else esdl file will be corrupted
-            if area_investment_cost > 0.0 or area_installation_cost > 0.0:
+            # TODO: discuss strange behaviour with Edwin - temporarily use of "True" in line below
+            if area_investment_cost > 0.0 or area_installation_cost > 0.0 or True:
                 kpis.kpi.append(
                     esdl.DoubleKPI(
                         value=area_investment_cost / 1.0e6,
@@ -642,7 +650,8 @@ class ScenarioOutput(HeatMixin):
                     )
                 )
             # Only update kpis if one of the costs > 0, else esdl file will be corrupted
-            if area_variable_opex_cost > 0.0 or area_fixed_opex_cost > 0.0:
+            # TODO: discuss strange behaviour with Edwin - temporarily use of "True" in line below
+            if area_variable_opex_cost > 0.0 or area_fixed_opex_cost > 0.0 or True:
                 kpis.kpi.append(
                     esdl.DoubleKPI(
                         value=area_variable_opex_cost / 1.0e6,
@@ -670,10 +679,9 @@ class ScenarioOutput(HeatMixin):
                 if total_energy_consumed_locally_wh[subarea.name] >= 0.0:
                     kpis.kpi.append(
                         esdl.DoubleKPI(
-                            value=estimated_energy_consumed_locally_wh[subarea.name],
-                            name="Local energy consumed [%]",
+                            value=round(estimated_energy_from_local_source_perc[subarea.name], 1),
+                            name="Estimated energy from local source(s) [%]",
                             quantityAndUnit=esdl.esdl.QuantityAndUnitType(
-                                # physicalQuantity=esdl.PhysicalQuantityEnum.POWER,
                                 unit=esdl.UnitEnum.PERCENT,
                                 multiplier=esdl.MultiplierEnum.NONE,
                             ),
@@ -681,10 +689,11 @@ class ScenarioOutput(HeatMixin):
                     )
                     kpis.kpi.append(
                         esdl.DoubleKPI(
-                            value=estimated_energy_consumed_regional_wh[subarea.name],
-                            name="Regional energy consumed [%]",
+                            value=round(
+                                estimated_energy_from_regional_source_perc[subarea.name], 1
+                            ),
+                            name="Estimated energy from regional source(s) [%]",
                             quantityAndUnit=esdl.esdl.QuantityAndUnitType(
-                                # physicalQuantity=esdl.PhysicalQuantityEnum.POWER,
                                 unit=esdl.UnitEnum.PERCENT,
                                 multiplier=esdl.MultiplierEnum.NONE,
                             ),
@@ -692,8 +701,8 @@ class ScenarioOutput(HeatMixin):
                     )
                     kpis.kpi.append(
                         esdl.DoubleKPI(
-                            value=total_energy_consumed_locally_wh[subarea.name] / 1.0e9,
-                            name="Area total energy consumed [GWh]",
+                            value=round(total_energy_consumed_locally_wh[subarea.name] / 1.0e9, 1),
+                            name="Total energy consumed [GWh]",
                             quantityAndUnit=esdl.esdl.QuantityAndUnitType(
                                 physicalQuantity=esdl.PhysicalQuantityEnum.ENERGY,
                                 unit=esdl.UnitEnum.WATTHOUR,
@@ -702,10 +711,10 @@ class ScenarioOutput(HeatMixin):
                         )
                     )
             except KeyError:
-                # Continue because this area does not have any energy consumption
+                # Do nothing because this area does not have any energy consumption
                 pass
 
-
+            # Create plots in the dashboard
             # Top level KPIs: Cost breakdown in a polygon area (for all assest grouped together)
             kpi_name = f"{subarea.name}: Asset cost breakdown [EUR]"
             if (area_installation_cost > 0.0 or area_investment_cost > 0.0) and (
