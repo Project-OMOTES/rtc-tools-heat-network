improved scaling and fix for multiple lines

src/rtctools_heat_network/heat_mixin.py
@@ -4305,17 +4305,18 @@ class HeatMixin(_HeadLossMixin, BaseComponentTypeMixin, CollocatedIntegratedOpti
                 parameters[f"{asset}.b_eff_coefficient"],
                 parameters[f"{asset}.c_eff_coefficient"],
                 n_lines=curve_fit_number_of_lines,
-                electrical_power_min=parameters[f"{asset}.minimum_load"],
+                electrical_power_min=0.,
                 electrical_power_max=self.bounds()[f"{asset}.ElectricityIn.Power"][1]
             )
             power_consumed_vect = ca.repmat(power_consumed, len(linear_coef_a))
             gas_mass_flow_out_vect = ca.repmat(gas_mass_flow_out, len(linear_coef_a))
             gass_mass_out_linearized_vect = linear_coef_a * power_consumed_vect + linear_coef_b
+            nominal = (self.variable_nominal(f"{asset}.Gas_mass_flow_out") * linear_coef_a * self.variable_nominal(f"{asset}.Power_consumed")) ** 0.5
             constraints.extend(
                 [
                     (
                         (gas_mass_flow_out_vect - gass_mass_out_linearized_vect) /
-                        self.variable_nominal(f"{asset}.Gas_mass_flow_out"),
+                        nominal,
                         -np.inf,
                         0.0,
                     ),
@@ -4338,7 +4339,7 @@ class HeatMixin(_HeadLossMixin, BaseComponentTypeMixin, CollocatedIntegratedOpti
                 f"{wp}.Electricity_source", ensemble_member
             )
             max = self.bounds()[f"{wp}.Electricity_source"][1].values
-            nominal = self.variable_nominal(f"{wp}.Electricity_source")
+            nominal = (self.variable_nominal(f"{wp}.Electricity_source")*np.median(max))**0.5
 
             constraints.append(((set_point * max - electricity_source) / nominal, 0.0, 0.0))
 

tests/models/unit_cases_electricity/electrolyzer/src/example.py
@@ -43,14 +43,17 @@ class _GoalsAndOptions:
         for demand in self.heat_network_components["electricity_demand"]:
             price_profile = f"{demand}.electricity_price"
             state = f"{demand}.Electricity_demand"
+            nominal = self.variable_nominal(state) * np.median(
+                self.get_timeseries(price_profile).values)
 
-            goals.append(RevenueGoal(state, price_profile, 1.e6))
+            goals.append(RevenueGoal(state, price_profile, nominal))
 
         for demand in self.heat_network_components["gas_demand"]:
             price_profile = f"{demand}.gas_price"
             state = f"{demand}.Gas_demand_mass_flow"
+            nominal=self.variable_nominal(state)*np.median(self.get_timeseries(price_profile).values)
 
-            goals.append(RevenueGoal(state, price_profile, 1.e6))
+            goals.append(RevenueGoal(state, price_profile, nominal))
 
         return goals
 
@@ -84,7 +87,7 @@ class MILPProblem(
 
     def solver_options(self):
         options = super().solver_options()
-        options["solver"] = "gurobi"
+        # options["solver"] = "highs"
 
         return options
 