cf5eff8b0f5b9ff8daa80aacab9cc34c7649170e
Jesus A. Rodriguez <jesusandres.rodriguez@deltares.nl>
2023-11-30 15:49:56 +0100

    including max heat supply assertion

--- tests/test_end_scenario_sizing_annualized.py
+++ tests/test_end_scenario_sizing_annualized.py
@@ -15,13 +15,28 @@ class TestEndScenarioSizingAnnualized(TestCase):
         import models.test_case_small_network_optional_assets_annualized.src.run_annualized as run_annualized
 
         base_folder = Path(run_annualized.__file__).resolve().parent.parent
-        # base_folder = Path(HeatProblemDiscAnnualizedCost.__file__).resolve().parent.parent
 
-        # First we check that the non annualized solution with discount=0 equals the one without discount computations and for one year
-        solution_1 = run_optimization_problem(HeatProblemDiscAnnualizedCost, base_folder=base_folder)
-        solution_2 = run_optimization_problem(HeatProblem, base_folder=base_folder)
+        # Non annualized objective value with discount=0 and technical life 1 
+        # year matches the objective value of the discounted problem
+        solution_1 = run_optimization_problem(HeatProblemDiscAnnualizedCost,
+                                              base_folder=base_folder)
+        solution_2 = run_optimization_problem(HeatProblem,
+                                              base_folder=base_folder)
+        np.testing.assert_allclose(solution_1.objective_value,
+                                   solution_2.objective_value)
 
-        np.testing.assert_allclose(solution_1.objective_value, solution_2.objective_value)
+        # To minimize investment cost, max heat supply from HeatProducer
+        # matches the installed capacity (max size).
+        # This also verifies that the constraint is respected
+        results = solution_1.extract_results()
+        heat_producers = [1, 2]
+        decimal = 3
+        for i in heat_producers:
+            capacity = np.max(results[f"HeatProducer_{i}.Heat_source"])
+            max_size = results[f"HeatProducer_{i}__max_size"]
+            err_msg_assert_2 = f"Max heat supply from HeatProducer_{i} is not sufficiently close to its supply capacity"
+            np.testing.assert_almost_equal(capacity, max_size, decimal, 
+                                           err_msg_assert_2)
 
 
         # Here we check that the discounted solution cheaper than the non-discounted one
@@ -29,13 +44,7 @@ class TestEndScenarioSizingAnnualized(TestCase):
         solution = run_optimization_problem(HeatProblemDiscAnnualizedCost, base_folder=base_folder)
 
         results = solution.extract_results()
-
-        # Check whether capacity investment matches maximum demand
-        # for d in solution.heat_network_components.get("demand", []):
-        #     target = solution.get_timeseries(f"{d}.target_heat_demand").values
-        #     np.testing.assert_allclose(target, results[f"{d}.Heat_demand"])
-
-
+        
 
 
 if __name__ == "__main__":
