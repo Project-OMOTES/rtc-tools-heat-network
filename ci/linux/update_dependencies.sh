3a9c0d9b1a0736f793ba378cbf3e75388bd57159
Sebastiaan la Fleur <sebastiaan.lafleur@tno.nl>
2023-12-01 09:45:39 +0100

    Update all dependencies when running ci/update_dependencies

--- ci/linux/update_dependencies.sh
+++ ci/linux/update_dependencies.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
 . .venv/bin/activate
-pip-compile --output-file=requirements.txt pyproject.toml
-pip-compile --extra=dev --output-file=dev-requirements.txt pyproject.toml
+pip-compile -U --output-file=requirements.txt pyproject.toml
+pip-compile -U --extra=dev --output-file=dev-requirements.txt pyproject.toml
--- ci/win32/update_dependencies.cmd
+++ ci/win32/update_dependencies.cmd
@@ -1,6 +1,6 @@
 
 pushd .
 cd /D "%~dp0"
-pip-compile --output-file=..\..\requirements.txt ..\..\pyproject.toml
-pip-compile --extra=dev --output-file=..\..\dev-requirements.txt ..\..\pyproject.toml
+pip-compile -U --output-file=..\..\requirements.txt ..\..\pyproject.toml
+pip-compile -U --extra=dev --output-file=..\..\dev-requirements.txt ..\..\pyproject.toml
 popd
