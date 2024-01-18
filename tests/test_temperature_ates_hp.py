f4ec407b98cda9b8dc750602d32ef1fdec2d566d
Femke Janssen <femke.janssen@tno.nl>
2024-01-18 17:32:51 +0100

    test to check the temperature state of the ates

---
+++ tests/test_temperature_ates_hp.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+from unittest import TestCase
+
+import numpy as np
+
+from rtctools.util import run_optimization_problem
+
+from utils_tests import demand_matching_test, energy_conservation_test, heat_to_discharge_test
+
+class TestAtesTemperature(TestCase):
+    """
+    Checks the constraints concerning the temperature changes in the ates as a result of heat loss and charging
+    """
+
+    def test_ates_temperature(self):
+        import models.ates_temperature.src.run_ates_temperature as run_ates_temperature
+        from models.ates_temperature.src.run_ates_temperature import HeatProblem
+
+        basefolder = Path(run_ates_temperature.__file__).resolve().parent.parent
+
+        solution = run_optimization_problem(HeatProblem, base_folder=basefolder)
+
+        results = solution.extract_results()
+        parameters = solution.parameters(0)
+        bounds = solution.bounds()
+
+        # check if
+        # - discrete temperature ates is equal to temperature of pipe (carrier) at inport during discharging
+        # - discrete temperature ates is equal or lower then temperature ates
+        #
+        # - Discharge heat and mass flow is corresponding to the temperature regime (flow rate remains the same, but heat changes)
+        ates_charging = results['Pipe1__flow_direct_var'] #=1 if charging
+        ates_temperature = results['ATES_cb47.Temperature_ates']
+        ates_temperature_disc = results['ATES_cb47__temperature_ates_disc']
+        carrier_temperature = results['41770304791669983859190_temperature']
+
+        np.testing.assert_array_less(ates_temperature_disc, ates_temperature)
+        np.testing.assert_allclose((1-ates_charging)*ates_temperature_disc, (1-ates_charging)*carrier_temperature)
+
+
+
+
+
+
+
+
+
+
