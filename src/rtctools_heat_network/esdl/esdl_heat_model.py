268be81f6450c3d1a5da660945fa4e7db7abe63e
Rojer <jim.rojer@tno.nl>
2024-02-23 15:03:38 +0100

    bug

--- src/rtctools_heat_network/esdl/esdl_heat_model.py
+++ src/rtctools_heat_network/esdl/esdl_heat_model.py
@@ -1204,7 +1204,7 @@ class AssetToHeatComponent(_AssetToComponentBase):
         modifiers = dict(
             Q_nominal=self._get_connected_q_nominal(asset),
             volume=asset.attributes["workingVolume"],
-            Gas_mass_flow=dict(min=hydrogen_specific_energy*asset.attributes["maxDischargeRate"], max=hydrogen_specific_energy*asset.attributes["maxChargeRate"])
+            Gas_tank_flow=dict(min=hydrogen_specific_energy*asset.attributes["maxDischargeRate"], max=hydrogen_specific_energy*asset.attributes["maxChargeRate"]),
             # TODO: Fix -> Gas network is currenlty non-limiting, mass flow is decoupled from the
             # volumetric flow
             # Gas_tank_flow=dict(
